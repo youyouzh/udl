9fe60a2ebcc683dd38f2839f292b77f8e832ab4c
uusama <1406558940@qq.com>
2023-07-20 18:22:54 +0800

    split show_images.

--- src/base/util.py
+++ src/base/util.py
@@ -203,3 +203,23 @@ def download_d2l_data(filename, cache_dir=None):
     with open(filepath, 'wb') as f:
         f.write(response.content)
     return filepath
+
+
+def show_images(images, num_rows, num_cols, titles=None, scale=1.5):
+    """绘制图像列表"""
+    fig_size = (num_cols * scale, num_rows * scale)
+    _, axes = plt.subplots(num_rows, num_cols, figsize=fig_size)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, images)):
+        if torch.is_tensor(img):
+            # 图片张量
+            ax.imshow(img.numpy())
+        else:
+            # PIL图片
+            ax.imshow(img)
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    plt.show()
+    return axes
--- src/learn/softmax_regression.py
+++ src/learn/softmax_regression.py
@@ -6,7 +6,7 @@ from torch import nn
 from torch.utils import data
 from torchvision import transforms
 
-from base.util import plt, plot, Accumulator, Animator, sgd
+from base.util import plt, plot, Accumulator, Animator, sgd, show_images
 from linear_regression import ManualLinearRegression
 
 DATA_LOADER_WORKERS = 4
@@ -17,26 +17,6 @@ def get_fashion_mnist_labels(labels):
     text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
                    'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
     return [text_labels[int(i)] for i in labels]
-
-
-def show_images(images, num_rows, num_cols, titles=None, scale=1.5):
-    """绘制图像列表"""
-    fig_size = (num_cols * scale, num_rows * scale)
-    _, axes = plt.subplots(num_rows, num_cols, figsize=fig_size)
-    axes = axes.flatten()
-    for i, (ax, img) in enumerate(zip(axes, images)):
-        if torch.is_tensor(img):
-            # 图片张量
-            ax.imshow(img.numpy())
-        else:
-            # PIL图片
-            ax.imshow(img)
-        ax.axes.get_xaxis().set_visible(False)
-        ax.axes.get_yaxis().set_visible(False)
-        if titles:
-            ax.set_title(titles[i])
-    plt.show()
-    return axes
 
 
 def load_data_fashion_mnist(batch_size, resize=None):
